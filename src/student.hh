c3ec6728bb580dd257bed2b70d5ad36a42a64d05
Carlos Aznarán Laos <caznaranl@uni.pe>
2022-05-08 11:48:44 -0500

    Completed

--- src/student.hh
+++ src/student.hh
@@ -3,23 +3,24 @@
 #include <cmath>
 
 // Test for exercise 1
-double result_1a = std::pow(5 - 19. / 7 + std::pow(2.5, 3), 2) ;
-double result_1b = 0; // TODO: Complete
-double result_1c = 0; // TODO: Complete
+double result_1a = std::pow(5 - 19. / 7 + std::pow(2.5, 3), 2);
+double result_1b = 7 * 3.1 + std::sqrt(120) / 5 - std::pow(15, 5. / 3);
+double result_1c = std::pow(1 / std::sqrt(75) + 73 / std::pow(3.1, 3), 1. / 4) + 55 * 0.41;
 
 // Test for exercise 2
-double result_2a = 0; // TODO: Complete
-double result_2b = 0; // TODO: Complete
+double const PI = std::acos(-1);
+double result_2a = std::sin(0.2 * PI) / std::cos(PI / 6) + std::tan(72 * PI / 180);
+double result_2b = std::pow(std::tan(64 * PI / 180) * std::cos(15 * PI / 180), 2) + std::pow(std::sin(37 * PI / 180) / std::cos(20 * PI / 180), 2);
 
 // Test for exercise 3
 float x = 6.5, y = 3.8;
 
 auto expr1 = [](float x, float y)
 {
-  return 0; // TODO: Complete
+  return std::pow(std::pow(x, 2) + std::pow(y, 2), 2. / 3) + x * y / (y - x);
 };
 
 auto expr2 = [](float x, float y)
 {
-  return 0; // TODO: Complete
+  return std::sqrt(x + y) / std::pow(x - y, 2) + 2 * std::pow(x, 2) - x * std::pow(y, 2);
 };
